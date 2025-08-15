8f8ebdff0a2a4ab1a373de1df4828d98cbec89bc
cruzortiz <cruzortiz099@gmail.com>
2025-08-15 12:59:59 -0400

    - updating template for receipt

--- lib/services/printer.dart
+++ lib/services/printer.dart
@@ -1,43 +1,30 @@
-import 'dart:io';
-import 'dart:typed_data';
-import 'package:flutter/foundation.dart';
-import 'package:printer_ui_win/services/ticket.dart';
-import 'package:windows_printer/windows_printer.dart';
+import "dart:io";
+import "dart:typed_data";
+import "package:flutter/foundation.dart";
+import "package:printer_ui_win/constants/env.dart";
+import "package:printer_ui_win/services/ticket.dart";
+import "package:windows_printer/windows_printer.dart";
 
-Future<void> printTicket(
-  String destination,
-  List<String> content,
-  String qrCode,
-) async {
+Future<void> printTicket(String destination, dynamic data) async {
+  // var (content, _, _) = generateTicketText(data);
+  Uint8List receipt = Uint8List.fromList(generateTicketReceipt(data).build());
   try {
-    var receipt = WPReceiptBuilder(wpPaperSize: WPPaperSize.mm80);
-    content.forEach((element) {
-      receipt.line(element);
-    });
-    receipt.addQRCode(qrCode);
-    receipt.drawer();
     await WindowsPrinter.printRichTextDocument(
       printerName: destination,
-      content: content.join("\n"),
+      content: receipt.toString(),
+      fontSize: int.parse(EnvVariables.receiptFontSize ?? "10"),
+      fontName: EnvVariables.receiptFontName ?? "Arial",
     );
   } catch (e) {
-    throw Exception('Error sending ticket to printer at $destination: $e');
+    throw Exception("Error sending ticket to printer at $destination: $e");
   }
 }
 
-Future<void> printNetworkTicket(
-  String destination,
-  List<String> content,
-  String qrCode,
-) async {
+Future<void> printNetworkTicket(String destination, dynamic data) async {
   try {
     // 1. Build the receipt data using the same logic as local printing.
-    var receipt = WPReceiptBuilder(wpPaperSize: WPPaperSize.mm80);
-    content.forEach((element) {
-      receipt.line(element);
-    });
-    receipt.addQRCode(qrCode);
-    final Uint8List data = Uint8List.fromList(receipt.build());
+    var receipt = generateTicketReceipt(data);
+    Uint8List receiptUintList = Uint8List.fromList(receipt.build());
 
     // 2. Connect to the network printer via raw TCP socket on port 9100.
     final socket = await Socket.connect(
@@ -47,25 +34,24 @@ Future<void> printNetworkTicket(
     );
 
     // 3. Send the data.
-    socket.add(data);
+    socket.add(receiptUintList);
     await socket.flush();
 
     // 4. Close the socket.
     await socket.close();
   } catch (e) {
     throw Exception(
-      'Error sending ticket to network printer at $destination: $e',
+      "Error sending ticket to network printer at $destination: $e",
     );
   }
 }
 
 Future<void> printInvoice(dynamic data, String destination) async {
   print("Printing invoice to $destination and data: $data");
-  var (content, qrImage, qrString) = generateTicketText(data);
   // Use a more robust regex to check for an IPv4 address format.
   if (RegExp(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$").hasMatch(destination)) {
-    await printNetworkTicket(destination, content, qrString);
+    await printNetworkTicket(destination, data);
   } else {
-    await printTicket(destination, content, qrString);
+    await printTicket(destination, data);
   }
 }
--- lib/services/ticket.dart
+++ lib/services/ticket.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
 import 'package:intl/intl.dart';
+import "package:printer_ui_win/constants/env.dart";
 import 'package:qr/qr.dart';
+import "package:windows_printer/windows_printer.dart";
 
 (QrImage, String) generate_afip_qr(
   dynamic data,
@@ -31,9 +33,9 @@ import 'package:qr/qr.dart';
   return (QrImage(QrCode(40, QrErrorCorrectLevel.L)..addData(url)), url);
 }
 
-(List<String>, QrImage, String) generateTicketText(dynamic data) {
+(List<String>, QrImage?, String?) generateTicketText(dynamic data) {
   List<String> lines = [];
-  var sep = "-" * 32;
+  var sep = "-" * 20;
   var qr_image = null;
   var qr_string = null;
   Map<String, dynamic> company = data["company"] ?? {};
@@ -50,7 +52,7 @@ import 'package:qr/qr.dart';
   }
   lines.add(sep);
   // Factura centrada
-  if (data["billing"] != null) {
+  if (data["billing"] != null && fields["voucher_type"] != null) {
     lines.add(fields["voucher_type"]?["Desc"]?.toString().toUpperCase() ?? "");
     lines.add("Código: ${fields["voucher_type"]?["Id"] ?? ""}");
     lines.add(sep);
@@ -78,7 +80,7 @@ import 'package:qr/qr.dart';
   lines.add(sep);
 
   // Detalle
-  lines.add("Cant x P.Unit        IMPORTE");
+  lines.add("Cant x P.Unit  |  IMPORTE");
   lines.add("Descripcion");
   lines.add(sep);
 
@@ -90,9 +92,10 @@ import 'package:qr/qr.dart';
 
     lines.add(
       "${cantidad.toStringAsFixed(2)} x ${precio.toStringAsFixed(2)}"
+      "  |  "
       "${subtotal.toStringAsFixed(2)}",
     );
-    if (tax != null && data["billing"] != null) {
+    if (tax > 0.0 && data["billing"] != null) {
       lines.add("IVA ${tax}%");
     }
 
@@ -106,7 +109,7 @@ import 'package:qr/qr.dart';
   lines.add(sep);
 
   // CAE y Vto
-  if (data["billing"] != null) {
+  if (data["billing"] != null && fields["cae"] != null) {
     lines.add("CAE: ${fields["cae"] ?? ""}");
     lines.add("Vto: ${fields["caef_ch_vto"] ?? ""}");
     if (data["billing"] != null) {
@@ -116,4 +119,102 @@ import 'package:qr/qr.dart';
 
   lines.add("\n" * 3);
   return (lines, qr_image, qr_string);
+}
+
+WPReceiptBuilder generateTicketReceipt(dynamic data) {
+  WPReceiptBuilder receiptBuilder = WPReceiptBuilder(
+    wpPaperSize: EnvVariables.receiptPaperSize == "58"
+        ? WPPaperSize.mm58
+        : WPPaperSize.mm80,
+  );
+
+  Map<String, dynamic> company = data["company"] ?? {};
+  Map<String, Map> fields = data["electronic_invoice"]?["fields"] ?? {};
+
+  // Encabezado
+  receiptBuilder.header(
+    [
+      "RAZON SOCIAL: ${company["name"]?.toString().toUpperCase()}",
+      "${data["client"]?["name"] ?? ""}",
+      "DIRECCION: ${company["address"] ?? ""}",
+      "C.U.I.T.: ${company["document_number"] ?? ""}",
+      "",
+      if (data["billing"] != null) "IIBB: ${fields["income_brut"] ?? "----"}",
+      if (data["billing"] != null)
+        "INICIO ACT: ${fields["activity_start_date"] ?? "----"}",
+      "",
+    ].join("\n"),
+  );
+  receiptBuilder.separator();
+
+  // Factura centrada
+  if (data["billing"] != null && fields["voucher_type"] != null) {
+    receiptBuilder.line(
+      fields["voucher_type"]?["Desc"]?.toString().toUpperCase() ?? "",
+    );
+    receiptBuilder.line("Código: ${fields["voucher_type"]?["Id"] ?? ""}");
+    receiptBuilder.separator();
+  }
+  // Datos de la factura
+  receiptBuilder.line("NRO: ${data["code"] ?? ""}");
+  receiptBuilder.line(
+    "CLIENTE: ${data["client"]?["name"] ?? "CONSUMIDOR FINAL"}",
+  );
+  receiptBuilder.line("FECHA: ${data["date"] ?? ""}");
+  receiptBuilder.line("HORA: ${data["hour"] ?? ""}");
+  Map seller = data["seller"] ?? {};
+  receiptBuilder.line("Vendedor: ${seller["name"] ?? ""}");
+  receiptBuilder.line("TIPO: ${data["invoice_type"]?["name"] ?? ""}");
+  if (data["billing"] != null) {
+    receiptBuilder.line("CONCEPTO: ${fields["concept_type"]?["Desc"] ?? ""}");
+  }
+  if (data["tables"] != null) {
+    for (var table in data["tables"]) {
+      receiptBuilder.line(
+        "MESA: ${table["name"] ?? ""} SALA ${table["living_room"]?["name"] ?? ""}",
+      );
+    }
+  }
+  receiptBuilder.separator();
+
+  // Detalle
+  receiptBuilder.item("Cant x P.Unit", "IMPORTE");
+  receiptBuilder.line("Descripcion");
+  receiptBuilder.separator();
+
+  for (var prod in data["products"] ?? []) {
+    var cantidad = double.parse("${prod["pivot"]?["amount"] ?? "0.0"}");
+    var precio = double.parse("${prod["pivot"]?["price"] ?? "0.0"}");
+    var subtotal = cantidad * precio;
+    var tax = double.parse("${prod["pivot"]?["taxe"] ?? "0.0"}");
+
+    receiptBuilder.item(
+      "${cantidad.toStringAsFixed(2)} x ${precio.toStringAsFixed(2)}",
+      "${subtotal.toStringAsFixed(2)}",
+    );
+    if (tax > 0.0 && data["billing"] != null) {
+      receiptBuilder.line("IVA ${tax}%");
+    }
+
+    receiptBuilder.line(prod["name"]?.toString().toUpperCase() ?? "");
+  }
+
+  receiptBuilder.separator();
+  receiptBuilder.totalAmt(
+    "${double.parse("${data['total'] ?? "0.0"}").toStringAsFixed(2)}",
+  );
+  receiptBuilder.separator();
+
+  // CAE y Vto
+  if (data["billing"] != null && fields["cae"] != null) {
+    receiptBuilder.line("CAE: ${fields["cae"] ?? ""}");
+    receiptBuilder.line("Vto: ${fields["caef_ch_vto"] ?? ""}");
+  }
+  if (data["billing"] != null) {
+    var (_, qrString) = generate_afip_qr(data, company, fields);
+    receiptBuilder.addQRCode(qrString);
+  }
+  receiptBuilder.blank(3);
+  receiptBuilder.cut(partial: false);
+  return receiptBuilder;
 }
