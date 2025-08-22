ca837acf1714d6e9531126b9dd2b96cc0157a596
cruzortiz <cruzortiz099@gmail.com>
2025-08-22 10:45:27 -0400

    - adding commanda print to model

--- lib/services/printer.dart
+++ lib/services/printer.dart
@@ -6,8 +6,9 @@ import "package:printer_ui_win/services/ticket.dart";
 import "package:windows_printer/windows_printer.dart";
 
 Future<void> printTicket(String destination, dynamic data) async {
-  var (content, _, _) = generateTicketText(data);
-  // var (content, _, _) = generateComandaText(data);
+  var (content, _, _) = (data["type"] ?? "").toLowerCase() == "comanda"
+      ? generateComandaText(data)
+      : generateTicketText(data);
   try {
     await WindowsPrinter.printRichTextDocument(
       printerName: destination,
